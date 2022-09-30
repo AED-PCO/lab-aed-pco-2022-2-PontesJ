580d36cc5141b9b7866fc5e1adecb86c2db7d95a
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2022-09-30 14:39:14 -0300

    Quick Sort

--- codigo/Laboratório 06/Quick Sort/Program.cs
+++ codigo/Laboratório 06/Quick Sort/Program.cs
@@ -12,9 +12,35 @@ namespace Quick_Sort
                 vetor[i] = rand.Next(0, 10);
             }
         }
-        static void quickSort(int[] vetor)
+        static void swap(int[] vetor, int inicio, int tam)
         {
-
+            int aux = vetor[inicio];
+            vetor[inicio] = vetor[tam];
+            vetor[tam] = aux;
+        }
+        static void quickSort(int[] vetor, int inicio, int tam)
+        {
+            if (tam > inicio)
+            {
+                int pivo = particiona(vetor, inicio, tam);
+                quickSort(vetor, inicio, pivo - 1);
+                quickSort(vetor, pivo + 1, tam);
+            }
+        }
+        static int particiona(int[] vetor, int inicio, int tam)
+        {
+            int pivo = vetor[tam];
+            int i = (inicio - 1);
+            for (int j = inicio; j <= tam - 1; j++)
+            {
+                if (vetor[j] < pivo)
+                {
+                    i++;
+                    swap(vetor, i, j);
+                }
+            }
+            swap(vetor, i + 1, tam);
+            return (i + 1);
         }
         static void Main(string[] args)
         {
@@ -26,7 +52,7 @@ namespace Quick_Sort
             Console.Write("\nO vetor: ");
             for (int i = 0; i < vetor.Length; i++) Console.Write("{0} ", vetor[i]);
             Console.Write("\nOrdenado: ");
-            quickSort(vetor);
+            quickSort(vetor, 0, tam - 1);
             for (int i = 0; i < vetor.Length; i++) Console.Write("{0} ", vetor[i]);
             Console.WriteLine("\n================================\n");
         }
